9516caf282ff8fb0bda5ad160939ac14065f3018
RDxsistema <113406124+RDxsistema@users.noreply.github.com>
2022-09-13 13:24:44 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbxyBIM7GiD33vT0WSMM2ez-8fH-H3lIUVDkFf2_G6vllqoymZOSRxgZzCtDbP3Givww/exec";
+const AS_URL_BASE = "https://script.google.com/a/~/macros/s/AKfycbxyBIM7GiD33vT0WSMM2ez-8fH-H3lIUVDkFf2_G6vllqoymZOSRxgZzCtDbP3Givww/exec";
